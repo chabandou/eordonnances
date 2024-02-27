a3467a47c81485425bcad617406ef3f0f81dd3bc
chabandou <chabandou@gmail.com>
2024-02-27 20:35:32 +0100

    added link

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Link href="/diseases">Go to diseases</Link>
 
      
     </main>
